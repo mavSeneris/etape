db59156469d7631f482936388263f4e49c93e099
Maverick Seneris <mavseneris@gmail.com>
2022-10-24 09:00:00 +0800

    added products cards

---
+++ src/products.js
@@ -0,0 +1,80 @@
+export default [
+    {
+        name: "Hex Pro Jersey",
+        price: 290.00,
+        description: "Aerodynamic race specific jersey",
+        altImage: "../src/assets/images/mens-alt6.png",
+        mainImage: "../src/assets/images/mens-jersey6.png"
+    },
+    {
+        name: "Hex Pro Jersey",
+        price: 290.00,
+        description: "Aerodynamic race specific jersey",
+        altImage: "../src/assets/images/mens-alt3.png",
+        mainImage: "../src/assets/images/mens-jersey3.png"
+    },
+    {
+        name: "Hex Pro Jersey",
+        price: 290.00,
+        description: "Aerodynamic race specific jersey",
+        altImage: "../src/assets/images/mens-alt3.png",
+        mainImage: "../src/assets/images/mens-jersey3.png"
+    },
+    {
+        name: "Hex Pro Jersey",
+        price: 290.00,
+        description: "Aerodynamic race specific jersey",
+        altImage: "../src/assets/images/mens-alt3.png",
+        mainImage: "../src/assets/images/mens-jersey3.png"
+    },
+    {
+        name: "Hex Pro Jersey",
+        price: 290.00,
+        description: "Aerodynamic race specific jersey",
+        altImage: "../src/assets/images/mens-alt3.png",
+        mainImage: "../src/assets/images/mens-jersey3.png"
+    },
+    {
+        name: "Hex Pro Jersey",
+        price: 290.00,
+        description: "Aerodynamic race specific jersey",
+        altImage: "../src/assets/images/mens-alt3.png",
+        mainImage: "../src/assets/images/mens-jersey3.png"
+    },
+    {
+        name: "Hex Pro Jersey",
+        price: 290.00,
+        description: "Aerodynamic race specific jersey",
+        altImage: "../src/assets/images/mens-alt3.png",
+        mainImage: "../src/assets/images/mens-jersey3.png"
+    },
+    {
+        name: "Hex Pro Jersey",
+        price: 290.00,
+        description: "Aerodynamic race specific jersey",
+        altImage: "../src/assets/images/mens-alt3.png",
+        mainImage: "../src/assets/images/mens-jersey3.png"
+    },
+    {
+        name: "Hex Pro Jersey",
+        price: 290.00,
+        description: "Aerodynamic race specific jersey",
+        altImage: "../src/assets/images/mens-alt3.png",
+        mainImage: "../src/assets/images/mens-jersey3.png"
+    },
+    {
+        name: "Hex Pro Jersey",
+        price: 290.00,
+        description: "Aerodynamic race specific jersey",
+        altImage: "../src/assets/images/mens-alt3.png",
+        mainImage: "../src/assets/images/mens-jersey3.png"
+    },
+    {
+        name: "Hex Pro Jersey",
+        price: 290.00,
+        description: "Aerodynamic race specific jersey",
+        altImage: "../src/assets/images/mens-alt3.png",
+        mainImage: "../src/assets/images/mens-jersey3.png"
+    },
+  
+]
